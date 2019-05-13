eb44ab21e5e37911b2f339f0726cafddb10f575e
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-05-13 17:19:43 +0200

    benchmark for the threadLocal delegate

---
+++ kroki-jmh/src/main/kotlin/com/github/kroki/benchmark/delegates/ThreadLocalDelegateBenchmark.kt
@@ -0,0 +1,19 @@
+package com.github.kroki.benchmark.delegates
+
+import com.github.kroki.delegates.threadLocal
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.Scope
+import org.openjdk.jmh.annotations.State
+import org.openjdk.jmh.infra.Blackhole
+
+@State(Scope.Benchmark)
+open class ThreadLocalDelegateBenchmark {
+
+    private val testValue by threadLocal { 1 }
+
+    @Benchmark
+    fun useThreadLocal(hole : Blackhole) {
+        hole.consume(testValue)
+    }
+
+}
