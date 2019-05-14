4de89008c34f9b1944f2bb266915e4a0d4204d03
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-05-14 22:59:24 +0200

    faster String.substringBetween
    20M ops / sec -> 27 M ops / sec on openjdk 8

--- kroki-utils/src/main/kotlin/com/github/kroki/strings/strings.kt
+++ kroki-utils/src/main/kotlin/com/github/kroki/strings/strings.kt
@@ -10,5 +10,23 @@ fun String.isUUID() = this.matches(uuidPattern)
 fun String.toUUID(): UUID =
     UUID.fromString(this)
 
-fun String.substringBetween(prefix: String, postfix: String): String =
-    this.substringAfter(prefix, "").substringBefore(postfix, "")
+private const val notFound = -1
+
+fun String.substringBetween(prefix: String, postfix: String): String {
+    val start = this.indexOf(prefix)
+    return if(start == notFound) {
+        val end = this.indexOf(postfix)
+        if(end == notFound) {
+            ""
+        } else {
+            this.substring(0, end)
+        }
+    } else {
+        val end = this.indexOf(postfix, start + prefix.length)
+        if (end == notFound) {
+            this.substring(start + prefix.length)
+        } else {
+            this.substring(start + prefix.length, end)
+        }
+    }
+}
