963e8f1ccaefc0aab5a273341fd9fc6514124dfc
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-05-14 07:38:32 +0200

    performance improvement on list join by pre-allocation and append to mutable list
    improved 5M ops/sec to 10M ops/spec

--- kroki-utils/src/main/kotlin/com/github/kroki/collections/lists.kt
+++ kroki-utils/src/main/kotlin/com/github/kroki/collections/lists.kt
@@ -1,13 +1,11 @@
 package com.github.kroki.collections
 
 fun <T> Collection<Collection<T>>.join(): List<T> {
-    var result = listOf<T>()
+    val result = ArrayList<T>(this.sumBy { it.size })
     this.forEach {
-        //performance: the += operation is relatively expensive, even with empty lists
-        //checking for empty improves throughput from 1.5M ops/sec to 1.8M ops/sec
         if (it.isNotEmpty()) {
-            result = result + it
+            result.addAll(it)
         }
     }
-    return result
+    return result.toList()
 }
