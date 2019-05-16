52f3661d42de2ec01293f33995075e50820aaee8
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-05-16 17:29:39 +0200

    size utilities from kerub

---
+++ kroki-utils/src/test/kotlin/com/github/kroki/size/SizeKtTest.kt
@@ -0,0 +1,14 @@
+package com.github.kroki.size
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class SizeKtTest {
+    @Test
+    fun checkSizes() {
+        assertEquals(1.KB, 1024.toBigInteger())
+        assertEquals(1.MB, 1024.KB)
+        assertEquals(2.TB, 2048.GB)
+        assertEquals(1.PB, 1024.TB)
+    }
+}
