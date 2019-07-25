5cfa60f9e237e180a6d9018f0d9c67d06aef997b
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-07-25 13:10:36 +0200

    operator functions for BigDecimal and BigInteger types

--- kroki-utils/src/main/kotlin/io/github/kerubistan/kroki/numbers/numbers.kt
+++ kroki-utils/src/main/kotlin/io/github/kerubistan/kroki/numbers/numbers.kt
@@ -3,4 +3,45 @@ package io.github.kerubistan.kroki.numbers
 import java.math.BigDecimal
 import java.math.BigInteger
 
+/*
+ BigDecmal
+ */
+
 operator fun BigDecimal.times(nr : BigInteger) = this.times(nr.toBigDecimal())
+
+operator fun BigDecimal.times(nr : Int) = this.times(nr.toBigDecimal())
+
+operator fun BigDecimal.times(nr : Long) = this.times(nr.toBigDecimal())
+
+operator fun BigDecimal.times(nr : Float) = this.times(nr.toBigDecimal())
+
+operator fun BigDecimal.times(nr : Double) = this.times(nr.toBigDecimal())
+
+operator fun BigDecimal.times(nr : Short) = this.times(nr.toInt().toBigDecimal())
+
+operator fun BigDecimal.times(nr : Byte) = this.times(nr.toInt().toBigDecimal())
+
+operator fun BigDecimal.compareTo(other: BigInteger) = this.compareTo(other.toBigDecimal())
+
+operator fun BigDecimal.compareTo(other: Int) = this.compareTo(other.toBigDecimal())
+
+operator fun BigDecimal.compareTo(other: Long) = this.compareTo(other.toBigDecimal())
+
+operator fun BigDecimal.compareTo(other: Short) = this.compareTo(other.toInt().toBigDecimal())
+
+operator fun BigDecimal.compareTo(other: Byte) = this.compareTo(other.toInt().toBigDecimal())
+
+operator fun BigDecimal.compareTo(other: Double) = this.compareTo(other.toBigDecimal())
+
+operator fun BigDecimal.compareTo(other: Float) = this.compareTo(other.toBigDecimal())
+
+
+/*
+ BigInteger
+ */
+
+operator fun BigInteger.times(multiplier : Double) = this.toBigDecimal() * multiplier.toBigDecimal()
+
+operator fun BigInteger.times(multiplier : Int) = this * multiplier.toBigInteger()
+
+operator fun BigInteger.compareTo(other : BigDecimal) = this.toBigDecimal().compareTo(other)
--- kroki-utils/src/test/kotlin/io/github/kerubistan/kroki/numbers/NumbersKtTest.kt
+++ kroki-utils/src/test/kotlin/io/github/kerubistan/kroki/numbers/NumbersKtTest.kt
@@ -1,13 +1,38 @@
 package io.github.kerubistan.kroki.numbers
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
 
 internal class NumbersKtTest {
 
     @Test
     fun times() {
         assertEquals("1.2".toBigDecimal(), "0.6".toBigDecimal() * 2.toBigInteger())
+        assertEquals("1.2".toBigDecimal(), "0.6".toBigDecimal() * 2)
+        assertEquals("1.2".toBigDecimal(), "0.6".toBigDecimal() * 2.toLong())
+        assertEquals(1.2, ("0.6".toBigDecimal() * 2.toFloat()).toDouble())
+        assertEquals(1.2, ("0.6".toBigDecimal() * 2.toDouble()).toDouble())
+        assertEquals("1.2".toBigDecimal(), "0.6".toBigDecimal() * 2.toShort())
+        assertEquals("1.2".toBigDecimal(), "0.6".toBigDecimal() * 2.toByte())
+        assertEquals(1024.toBigInteger(), 512.toBigInteger() * 2)
+        assertEquals(1024.0.toBigDecimal(), 512.toBigInteger() * 2.0)
     }
+
+    @Test
+    fun compareTo() {
+
+        assertTrue("1.0001".toBigDecimal() > 1.toBigInteger())
+        assertTrue("0.999999".toBigDecimal() < 1.toBigInteger())
+        assertTrue("1.0001".toBigDecimal() > 1)
+        assertTrue("1.0001".toBigDecimal() > 1.toLong())
+        assertTrue("1.0001".toBigDecimal() > 1.toShort())
+        assertTrue("1.0001".toBigDecimal() > 1.toByte())
+        assertTrue("1.01".toBigDecimal() > 1.001)
+        assertTrue("1.01".toBigDecimal() > 1.001.toFloat())
+
+
+        assertTrue(1.toBigInteger() < "1.0001".toBigDecimal())
+    }
+
 }
