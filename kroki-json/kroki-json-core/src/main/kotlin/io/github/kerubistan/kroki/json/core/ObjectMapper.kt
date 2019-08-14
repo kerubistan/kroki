65caf9a627aa2619cb0dc2ea1e4706ab51a9f618
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-08-14 07:51:45 +0200

    skeleton of json module
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

---
+++ kroki-json/kroki-json-core/src/main/kotlin/io/github/kerubistan/kroki/json/core/ObjectMapper.kt
@@ -0,0 +1,9 @@
+package io.github.kerubistan.kroki.json.core
+
+class ObjectMapper {
+
+    fun objectToString(obj : Any) : String = TODO()
+
+    fun <T> stringToObject(input : String) : T = TODO()
+
+}
