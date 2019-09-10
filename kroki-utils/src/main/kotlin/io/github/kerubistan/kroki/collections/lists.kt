3e7960154a83e43f2d3930cb447eb6bfe2ad2ae2
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-09-10 22:00:44 +0200

    first implementation of percentile extension function

--- kroki-utils/src/main/kotlin/io/github/kerubistan/kroki/collections/lists.kt
+++ kroki-utils/src/main/kotlin/io/github/kerubistan/kroki/collections/lists.kt
@@ -9,3 +9,18 @@ fun <T> Collection<Collection<T>>.join(): List<T> {
     }
     return result.toList()
 }
+
+inline fun <T, E : Comparable<E>> Iterable<T>.percentile(percentile: Double, crossinline expression: (T) -> E): E {
+    require(percentile < 100) {
+        "Percentile must be less than 100. Actual: $percentile"
+    }
+    require(percentile > 0) {
+        "Percentile must be more than 0. Actual: $percentile"
+    }
+    val ordered = this.sortedBy(expression)
+    require(ordered.isNotEmpty()) {
+        "There is nothing to calculate with. "
+    }
+
+    return expression(ordered[(ordered.size * (percentile / 100)).toInt()])
+}
--- kroki-utils/src/test/kotlin/io/github/kerubistan/kroki/collections/ListsKtTest.kt
+++ kroki-utils/src/test/kotlin/io/github/kerubistan/kroki/collections/ListsKtTest.kt
@@ -1,6 +1,7 @@
 package io.github.kerubistan.kroki.collections
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertEquals
 
 internal class ListsKtTest {
@@ -9,6 +10,27 @@ internal class ListsKtTest {
         assertEquals(listOf("A", "B", "C", "D"), listOf(listOf("A"), listOf("B", "C", "D")).join())
         assertEquals(listOf("A", "B", "C", "D"), listOf(listOf("A", "B"), listOf("C", "D")).join())
         assertEquals(listOf(), listOf(listOf(), listOf<String>()).join())
+    }
+
+    @Test
+    fun percentile() {
+        assertThrows<IllegalArgumentException> {
+            (0..100).toList().shuffled().percentile(-1.0) {it}
+        }
+        assertThrows<IllegalArgumentException> {
+            (0..100).toList().shuffled().percentile(0.0) {it}
+        }
+        assertThrows<IllegalArgumentException> {
+            (0..100).toList().shuffled().percentile(100.0) {it}
+        }
+        assertThrows<IllegalArgumentException> {
+            (0..100).toList().shuffled().percentile(101.0) {it}
+        }
+        assertThrows<IllegalArgumentException> {
+            listOf<Int>().percentile(90.0) {it}
+        }
+
+        assertEquals(99, (0..100).toList().shuffled().percentile(99.0) {it})
     }
 
 }
