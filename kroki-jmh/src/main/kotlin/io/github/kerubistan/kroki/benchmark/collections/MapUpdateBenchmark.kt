e4628838dabf52c48fcbf5810b2778edc64ef165
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-11-15 07:30:44 +0100

    Benchmark for map.update
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

---
+++ kroki-jmh/src/main/kotlin/io/github/kerubistan/kroki/benchmark/collections/MapUpdateBenchmark.kt
@@ -0,0 +1,30 @@
+package io.github.kerubistan.kroki.benchmark.collections
+
+import io.github.kerubistan.kroki.collections.update
+import org.openjdk.jmh.annotations.*
+import org.openjdk.jmh.infra.Blackhole
+
+@State(Scope.Benchmark)
+open class MapUpdateBenchmark {
+
+    @Param("0","1","2","4","8","16","32","64","128","256","512","1024")
+    var size = 0
+
+    lateinit var map : Map<String, String>
+
+    @Setup
+    fun setup() {
+        map = (0..size).map { index -> "key-$index" to "value-$index" }.toMap()
+    }
+
+    @Benchmark
+    fun hit(hole : Blackhole) {
+        hole.consume(map.update("key-0") { value -> "$value updated" })
+    }
+
+    @Benchmark
+    fun miss(hole : Blackhole) {
+        hole.consume(map.update("key-MISS") { value -> "$value updated" })
+    }
+
+}
