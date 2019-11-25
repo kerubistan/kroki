93d6636267093f314d6025ae9045cb3cf15b71ff
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-11-25 17:34:14 +0100

    benchmark for list.replace extension function
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

---
+++ kroki-jmh/src/main/kotlin/io/github/kerubistan/kroki/benchmark/collections/ReplaceBenchmark.kt
@@ -0,0 +1,29 @@
+package io.github.kerubistan.kroki.benchmark.collections
+
+import io.github.kerubistan.kroki.collections.replace
+import org.openjdk.jmh.annotations.*
+import org.openjdk.jmh.infra.Blackhole
+
+@State(Scope.Benchmark)
+open class ReplaceBenchmark {
+
+    @Param("0", "1", "2", "4", "8", "16", "32")
+    var listSize = 0
+
+    @Param("2", "4")
+    var replaceItemsPer = 0
+
+    lateinit var list: List<Boolean>
+
+    @Setup
+    fun setup() {
+        list = (0 until listSize).map { index ->
+            index % replaceItemsPer == 0
+        }
+    }
+
+    @Benchmark
+    fun run(hole: Blackhole) {
+        hole.consume(list.replace(filter = { !it }, replacer = { !it }))
+    }
+}
