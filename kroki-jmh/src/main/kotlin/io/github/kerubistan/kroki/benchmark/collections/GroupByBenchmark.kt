1b229f60a5cb0cf45a1b6b6ab723869d21cfc9b6
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2019-11-25 07:58:02 +0100

    benchmark for groupsBy
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

---
+++ kroki-jmh/src/main/kotlin/io/github/kerubistan/kroki/benchmark/collections/GroupByBenchmark.kt
@@ -0,0 +1,41 @@
+package io.github.kerubistan.kroki.benchmark.collections
+
+import io.github.kerubistan.kroki.collections.groupsBy
+import org.openjdk.jmh.annotations.*
+import org.openjdk.jmh.infra.Blackhole
+import kotlin.random.Random
+
+@State(Scope.Benchmark)
+open class GroupByBenchmark {
+
+    @Param("0", "1", "2", "4", "8", "16", "32", "64", "128", "256")
+    var listSize = 0
+
+    @Param("0", "1", "2", "4", "8", "16", "32", "64")
+    var nrOfGroups = 0
+
+    @Param("0", "1", "2", "4", "8")
+    var groupsPerItem = 0
+
+    data class Product(val name : String, val categories : Set<String>)
+
+    lateinit var list : List<Product>
+
+    @Setup
+    fun setup() {
+        val random = Random
+        list = (0 until listSize).map { index ->
+            Product(
+                name = "Product $index",
+                categories = (0 until groupsPerItem).map { _ ->
+                    "category-${random.nextInt(nrOfGroups)})"
+                }.toSet()
+            )
+        }
+    }
+
+    @Benchmark
+    fun run(hole : Blackhole) {
+        hole.consume(list.groupsBy(Product::categories))
+    }
+}
