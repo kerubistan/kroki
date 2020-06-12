8fb2dc1ad0c41e0e68c2b992b4273ebff85d71e8
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2020-06-12 09:20:47 +0200

    benchmark for all 4 formatting modes
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- kroki-jmh/src/main/kotlin/io/github/kerubistan/kroki/benchmark/xml/XmlGenerationBenchmark.kt
+++ kroki-jmh/src/main/kotlin/io/github/kerubistan/kroki/benchmark/xml/XmlGenerationBenchmark.kt
@@ -1,14 +1,27 @@
 package io.github.kerubistan.kroki.benchmark.xml
 
+import io.github.kerubistan.kroki.FormatMode
 import io.github.kerubistan.kroki.xml
-import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.*
 import org.openjdk.jmh.infra.Blackhole
 
+@State(Scope.Benchmark)
 open class XmlGenerationBenchmark {
+
+	@Param("COMPACT", "PRETTY_TABS", "PRETTY_BIG_SPACE_NAZI", "PRETTY_SMALL_SPACE_NAZI")
+	var formatModeStr: String = ""
+
+	lateinit var formatMode: FormatMode
+
+	@Setup
+	fun setup() {
+		formatMode = FormatMode.valueOf(formatModeStr)
+	}
+
 	@Benchmark
 	fun generate(hole: Blackhole) {
 		hole.consume(
-			xml(root = "root") {
+			xml(formatMode = formatMode, root = "root") {
 				!"comment"
 				tag("tag", "attribute" to "value")
 			}.reader().readText()
