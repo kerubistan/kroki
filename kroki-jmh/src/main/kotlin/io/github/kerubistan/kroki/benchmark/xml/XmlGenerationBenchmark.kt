ff069579baac4c15a21c013a33430ae9602a7920
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2020-06-12 16:38:44 +0200

    benchmark modified for different xml sizes
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- kroki-jmh/src/main/kotlin/io/github/kerubistan/kroki/benchmark/xml/XmlGenerationBenchmark.kt
+++ kroki-jmh/src/main/kotlin/io/github/kerubistan/kroki/benchmark/xml/XmlGenerationBenchmark.kt
@@ -1,6 +1,7 @@
 package io.github.kerubistan.kroki.benchmark.xml
 
 import io.github.kerubistan.kroki.FormatMode
+import io.github.kerubistan.kroki.XmlBuilder
 import io.github.kerubistan.kroki.xml
 import org.openjdk.jmh.annotations.*
 import org.openjdk.jmh.infra.Blackhole
@@ -11,20 +12,47 @@ open class XmlGenerationBenchmark {
 	@Param("COMPACT", "PRETTY_TABS", "PRETTY_BIG_SPACE_NAZI", "PRETTY_SMALL_SPACE_NAZI")
 	var formatModeStr: String = ""
 
+	@Param("blank", "tiny", "medium", "big", "huge")
+	var size: String = ""
+
 	lateinit var formatMode: FormatMode
+
+	lateinit var builder: XmlBuilder.() -> Unit
+
+	private val blankBuilder : XmlBuilder.() -> Unit = {}
+
+	private val tinyBuilder : XmlBuilder.() -> Unit = {
+		!"comment"
+		tag("tag", "attribute" to "value")
+	}
+
+	fun builder(size : Int) : XmlBuilder.() -> Unit {
+		return {
+			repeat(size) {
+				!"comment"
+				tag("tag", "attribute" to "value")
+			}
+		}
+	}
 
 	@Setup
 	fun setup() {
 		formatMode = FormatMode.valueOf(formatModeStr)
+		builder = when(size) {
+			"blank" -> blankBuilder
+			"tiny" -> tinyBuilder
+			"medium" -> builder(10)
+			"big" -> builder(100)
+			"huge" -> builder(1000)
+			else -> TODO()
+		}
 	}
+
 
 	@Benchmark
 	fun generate(hole: Blackhole) {
 		hole.consume(
-			xml(formatMode = formatMode, root = "root") {
-				!"comment"
-				tag("tag", "attribute" to "value")
-			}.reader().readText()
+			xml(formatMode = formatMode, root = "root", builder = builder).reader().readText()
 		)
 	}
 }
