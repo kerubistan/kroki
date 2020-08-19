e30a5d83a9e1d06c2100424f395ecf6411cef15d
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2020-08-19 07:44:23 +0200

    tests for NullOutputStream
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- kroki-utils/src/main/kotlin/io/github/kerubistan/kroki/io/NullOutputStream.kt
+++ kroki-utils/src/main/kotlin/io/github/kerubistan/kroki/io/NullOutputStream.kt
@@ -6,7 +6,7 @@ import java.io.OutputStream
  * An OutputStream that writes nowhere, a kotlin equivalent of /dev/null
  */
 object NullOutputStream : OutputStream() {
-	override fun write(p0: ByteArray, p1: Int, p2: Int) {
+	override fun write(data: ByteArray, p1: Int, p2: Int) {
 		// intentionally blank
 	}
 	override fun flush() {
