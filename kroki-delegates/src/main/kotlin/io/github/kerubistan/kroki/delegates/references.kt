e45de852772a87ef65b87b3f175936583da6d81f
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2020-08-22 21:33:36 +0200

    delegates split into files by functional area
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- kroki-delegates/src/main/kotlin/io/github/kerubistan/kroki/delegates/references.kt
+++ kroki-delegates/src/main/kotlin/io/github/kerubistan/kroki/delegates/references.kt
@@ -6,23 +6,6 @@ import java.lang.ref.SoftReference
 import java.lang.ref.WeakReference
 import kotlin.reflect.KProperty
 
-private class FunctionLiteralThreadLocal<T>(private val initializer: () -> T) : ThreadLocal<T>() {
-	override fun initialValue(): T = initializer()
-}
-
-class ThreadLocalDelegate<T>(initializer: () -> T) {
-
-	private val threadLocal = FunctionLiteralThreadLocal(initializer)
-
-	operator fun getValue(obj: Any?, property: KProperty<*>): T = threadLocal.get()
-}
-
-/**
- * Allows having one instance created by thread.
- * @param initializer a function literal that initializes the instance for the thread
- */
-fun <T> threadLocal(initializer: () -> T) = ThreadLocalDelegate(initializer)
-
 /**
  * Delegate interface for all kind of reference (weak, soft) delegates.
  */
