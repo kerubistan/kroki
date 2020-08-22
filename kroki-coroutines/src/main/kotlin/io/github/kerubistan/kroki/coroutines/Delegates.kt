77830bbb64be5d9738045d9d6d04bda538d00053
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2020-08-22 23:24:41 +0200

    doco fixes
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- kroki-coroutines/src/main/kotlin/io/github/kerubistan/kroki/coroutines/Delegates.kt
+++ kroki-coroutines/src/main/kotlin/io/github/kerubistan/kroki/coroutines/Delegates.kt
@@ -5,7 +5,7 @@ import java.io.Serializable
 import kotlin.reflect.KProperty
 
 /**
- * Delegate interface for all kind of reference (weak, soft) delegates.
+ * Delegate interface for all kind of delegates.
  */
 interface EagerDelegate<T> : Serializable {
 	val value: T
@@ -28,8 +28,9 @@ private class EagerDelegateImpl<T>(
 
 /**
  * Creates an eager delegate.
- * An eager delegate will not immediately perform the calculation and therefore may avoid CPU usage on the
+ * An eager delegate will not block the thread to perform the calculation and therefore may avoid CPU usage on the
  * thread where it is created, but it starts a co-routine to perform the operation.
  */
 fun <T> eager(scope: CoroutineScope = GlobalScope, initializer: () -> T): EagerDelegate<T> =
 	EagerDelegateImpl(initializer, scope)
+
