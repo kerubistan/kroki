399f8f65149155646ee4bdadee38093fbab19644
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2020-08-30 14:55:47 +0200

    `cached` delegate
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- kroki-coroutines/src/test/kotlin/io/github/kerubistan/kroki/coroutines/DelegatesKtTest.kt
+++ kroki-coroutines/src/test/kotlin/io/github/kerubistan/kroki/coroutines/DelegatesKtTest.kt
@@ -2,6 +2,9 @@ package io.github.kerubistan.kroki.coroutines
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.net.UnknownHostException
+import kotlin.test.assertTrue
 
 internal class DelegatesKtTest {
 
@@ -16,4 +19,50 @@ internal class DelegatesKtTest {
 		assertEquals(original.sorted(), eagerOrdered)
 		assertEquals(original.sorted(), eagerOrdered)
 	}
+
+	@Test
+	fun cachedDelegate() {
+
+		var cntr = 0
+		val data by cached { ++cntr }
+
+		assertEquals(1, data)
+		assertEquals(1, data)
+		assertEquals(1, cntr)
+
+	}
+
+	@Test
+	fun cachedDelegateErrorHandling() {
+
+		val errors = mutableListOf<Throwable>()
+		var cntr = 0
+		val data by cached(retryOnFail = 10, errorHandler = { errors.add(it) }) {
+			if (cntr++ > 2)
+				1
+			else throw UnknownHostException("blah.example.com not known")
+		}
+
+		assertEquals(1, data)
+		assertEquals(1, data)
+		assertEquals(3, errors.size)
+		assertTrue(errors.all { it is UnknownHostException })
+
+	}
+
+	@Test
+	fun cachedDelegateErrorHandlingFail() {
+
+		var cntr = 0
+		val data by cached(retryOnFail = 1) {
+			if (cntr++ > 2)
+				1
+			else throw UnknownHostException("blah.example.com not known")
+		}
+
+		assertThrows<UnknownHostException> { println(data) }
+
+	}
+
+
 }
--- kroki-utils/src/main/kotlin/io/github/kerubistan/kroki/time/time.kt
+++ kroki-utils/src/main/kotlin/io/github/kerubistan/kroki/time/time.kt
@@ -4,3 +4,24 @@ package io.github.kerubistan.kroki.time
  * UTC time from the system expressed in a short and human-readable way.
  */
 fun now() = System.currentTimeMillis()
+
+val Int.H : Long
+	get() = this * 60.MIN
+
+val Int.MIN : Long
+	get() = this * 60.SEC
+
+val Int.SEC : Long
+	get() = this * 1000.MS
+
+val Int.MS : Long
+	get() {
+		require(this >= 0) { " Duration must be positive. Given: $this" }
+		return this.toLong()
+	}
+
+val Int.D : Long
+	get() = this * 24.H
+
+val Int.W : Long
+	get() = this * 7.D
