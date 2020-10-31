6a5314fb2a052120c6b8c8fa4d9321f7f0a0e653
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2020-10-31 02:17:11 +0100

    instance cache: use weak hashmap
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- kroki-flyweight/kroki-flyweight-lib/src/main/kotlin/io/github/kerubistan/kroki/flyweight/GlobalInstanceCache.kt
+++ kroki-flyweight/kroki-flyweight-lib/src/main/kotlin/io/github/kerubistan/kroki/flyweight/GlobalInstanceCache.kt
@@ -1,14 +1,15 @@
 package io.github.kerubistan.kroki.flyweight
 
+import java.util.*
 import kotlin.reflect.KClass
 
 object GlobalInstanceCache : InstanceCache {
 
-	private val caches: MutableMap<KClass<*>, MutableMap<*, *>> = mutableMapOf()
+	private val caches: MutableMap<KClass<*>, MutableMap<*, *>> = WeakHashMap()
 
 	override fun <T : Any> cacheForClass(clazz: KClass<T>): MutableMap<T, T> {
 		val cache =
-			caches[clazz] ?: mutableMapOf<T, T>().apply { synchronized(this@GlobalInstanceCache) { caches[clazz] = this } }
+			caches[clazz] ?: WeakHashMap<T, T>().apply { synchronized(this@GlobalInstanceCache) { caches[clazz] = this } }
 		return cache as MutableMap<T, T>
 	}
 }
