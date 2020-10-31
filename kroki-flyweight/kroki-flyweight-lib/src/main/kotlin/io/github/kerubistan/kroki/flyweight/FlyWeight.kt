f601b3e2f091d43dfe00aeb41a6956b2ef617b28
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2020-10-31 14:00:36 +0100

    cleanup and documentation
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- kroki-flyweight/kroki-flyweight-lib/src/main/kotlin/io/github/kerubistan/kroki/flyweight/FlyWeight.kt
+++ kroki-flyweight/kroki-flyweight-lib/src/main/kotlin/io/github/kerubistan/kroki/flyweight/FlyWeight.kt
@@ -3,13 +3,16 @@ package io.github.kerubistan.kroki.flyweight
 import io.github.kerubistan.kroki.flyweight.annotations.IgnoreFlyWeight
 import kotlin.reflect.KFunction
 
+/**
+ * Creates a de-duplicated copy of the object, which will be equal (==) to the original.
+ */
 @Suppress("UNCHECKED_CAST", "IMPLICIT_CAST_TO_ANY")
 fun <T : Any> T.flyWeight(instanceCache: InstanceCache = GlobalInstanceCache): T =
 	when {
 		this is String ->
 			this.intern()
 		this.javaClass.kotlin.isData ->
-			flyWeightDataObject(instanceCache)
+			flyWeightDataInstance(instanceCache)
 		this is Set<*> ->
 			this.map { it?.flyWeight(instanceCache) }.toSet()
 		this is List<*> -> {
@@ -23,8 +26,11 @@ fun <T : Any> T.flyWeight(instanceCache: InstanceCache = GlobalInstanceCache): T
 		else -> this
 	} as T
 
+/**
+ * Makes a copy of an instance of kotlin data class, de-duplicating each property.
+ */
 @Suppress("UNCHECKED_CAST")
-private fun <T : Any> T.flyWeightDataObject(instanceCache: InstanceCache): T {
+private fun <T : Any> T.flyWeightDataInstance(instanceCache: InstanceCache): T {
 	val memberFunctions = this.javaClass.kotlin.members.filterIsInstance<KFunction<*>>()
 	val copy = memberFunctions.single { it.name == "copy" }
 	val deDuplicatedFields = memberFunctions
@@ -33,7 +39,7 @@ private fun <T : Any> T.flyWeightDataObject(instanceCache: InstanceCache): T {
 		.mapIndexed { index, component ->
 			var componentValue = component.call(this)
 			copy.parameters[index + 1] to if (componentValue != null) {
-				if (noFlyWeightType(componentValue, component)) {
+				if (noFlyWeightType(componentValue)) {
 					componentValue
 				} else {
 					val componentCache = instanceCache.cacheForClass(componentValue.javaClass.kotlin)
@@ -50,6 +56,10 @@ private fun <T : Any> T.flyWeightDataObject(instanceCache: InstanceCache): T {
 	return copy.callBy(deDuplicatedFields) as T
 }
 
+/**
+ * Classes that should not be considered to be de-duplicated, either because they have too few possible values
+ * or they are de-duplicated by the runtime (such as Integer.valueOf() caches a number of instances for the first some).
+ */
 private val noFlyWeightTypes = setOf(
 	Boolean::class,
 	Short::class,
@@ -59,7 +69,10 @@ private val noFlyWeightTypes = setOf(
 	Long::class
 )
 
-private fun noFlyWeightType(componentValue: Any, component: KFunction<*>) =
+/**
+ * Checks if the value should be ignored by de-duplication logic.
+ */
+private fun noFlyWeightType(componentValue: Any) =
 	componentValue.javaClass.isEnum
-			|| component.javaClass.kotlin in noFlyWeightTypes
+			|| componentValue.javaClass.kotlin in noFlyWeightTypes
 			|| componentValue.javaClass.annotations.any { it is IgnoreFlyWeight }
