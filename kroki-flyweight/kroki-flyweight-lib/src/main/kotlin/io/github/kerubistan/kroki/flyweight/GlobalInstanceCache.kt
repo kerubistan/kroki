1c4a3d7b4d4e8c507d084aebbb5253cae2ca80b3
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2021-02-24 07:42:29 +0100

    suppress warning
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- kroki-flyweight/kroki-flyweight-lib/src/main/kotlin/io/github/kerubistan/kroki/flyweight/GlobalInstanceCache.kt
+++ kroki-flyweight/kroki-flyweight-lib/src/main/kotlin/io/github/kerubistan/kroki/flyweight/GlobalInstanceCache.kt
@@ -7,6 +7,7 @@ object GlobalInstanceCache : InstanceCache {
 
 	private val caches: MutableMap<KClass<*>, MutableMap<*, *>> = WeakHashMap()
 
+	@Suppress("UNCHECKED_CAST")
 	override fun <T : Any> cacheForClass(clazz: KClass<T>): MutableMap<T, T> {
 		val cache =
 			caches[clazz] ?: WeakHashMap<T, T>().apply { synchronized(this@GlobalInstanceCache) { caches[clazz] = this } }
