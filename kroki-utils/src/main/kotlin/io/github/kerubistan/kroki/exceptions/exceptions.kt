07ba8e1a8f078f44ff37ddbf8bdc4baf77cf9d52
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2021-08-02 16:45:51 +0200

    fun getStackTraceAsString -> val stackTraceText, old method deprecated
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- kroki-utils/src/main/kotlin/io/github/kerubistan/kroki/exceptions/exceptions.kt
+++ kroki-utils/src/main/kotlin/io/github/kerubistan/kroki/exceptions/exceptions.kt
@@ -3,19 +3,29 @@ package io.github.kerubistan.kroki.exceptions
 import io.github.kerubistan.kroki.io.EasyStringWriter
 import java.io.PrintWriter
 
-fun Throwable.getStackTraceAsString(): String =
-	EasyStringWriter(256).use { stringWriter ->
+/**
+ * The stack trace of the throwable as string.
+ */
+val Throwable.stackTraceText: String
+	get() = EasyStringWriter(256).use { stringWriter ->
 		PrintWriter(stringWriter).use {
 			this.printStackTrace(it)
 		}
 		stringWriter.toString()
 	}
 
+@Deprecated(
+	replaceWith = ReplaceWith("stackTraceText", "io.github.kerubistan.kroki.exceptions.stackTraceText"),
+	message = "stackTraceText reads way better",
+	level = DeprecationLevel.WARNING
+)
+fun Throwable.getStackTraceAsString(): String = this.stackTraceText
+
 /**
  * Sometimes, typically with remote calls things can go bad, and it is OK
  * to try again. Such cases are like loading an RSS feed,
  */
-inline fun <T> insist(tries: Int, onError : (t: Throwable) -> Unit, action: () -> T): T {
+inline fun <T> insist(tries: Int, onError: (t: Throwable) -> Unit, action: () -> T): T {
 	for (attempt in 0 until tries) {
 		try {
 			return action()
