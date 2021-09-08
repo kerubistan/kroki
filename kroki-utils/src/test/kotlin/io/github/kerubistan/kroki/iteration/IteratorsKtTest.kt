98b18796fc5c5b9ac231e1a6a58be68a0a2f6fa3
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2021-09-08 15:37:48 +0200

    performance improvement on the attribute map generation
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- kroki-utils/src/test/kotlin/io/github/kerubistan/kroki/iteration/IteratorsKtTest.kt
+++ kroki-utils/src/test/kotlin/io/github/kerubistan/kroki/iteration/IteratorsKtTest.kt
@@ -1,22 +1,1 @@
-package io.github.kerubistan.kroki.iteration
-
-import org.junit.Assert.assertEquals
-import org.junit.jupiter.api.Test
-
-class IteratorsKtTest {
-
-	@Test
-	fun map() {
-		assertEquals(
-			listOf(2, 3, 4, 5, 6),
-			listOf(1, 2, 3, 4, 5).iterator().map { it + 1 }.toList()
-		)
-	}
-
-	@Test
-	fun toList() {
-		assertEquals(listOf(1, 2, 3, 4), listOf(1, 2, 3, 4).iterator().toList())
-	}
-
-
-}
+package io.github.kerubistan.kroki.iterationimport org.junit.Assert.assertEqualsimport org.junit.jupiter.api.Testclass IteratorsKtTest {	@Test	fun map() {		assertEquals(			listOf(2, 3, 4, 5, 6),			listOf(1, 2, 3, 4, 5).iterator().map { it + 1 }.toList()		)	}	@Test	fun toList() {		assertEquals(listOf(1, 2, 3, 4), listOf(1, 2, 3, 4).iterator().toList())	}	@Test	fun iteratorToMap() {		assertEquals(			mapOf("f" to "foo", "b" to "bar"),			listOf("foo", "bar").iterator().toMap { substring(0, 1) to this }		)	}}
