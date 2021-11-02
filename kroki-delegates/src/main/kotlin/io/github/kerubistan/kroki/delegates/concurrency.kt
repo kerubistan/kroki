bb3f24b75c943b7b2f4c4a3662de12c40ddf3bce
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2021-11-02 16:44:24 +0100

    mutable threadLocal variable delegates
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- kroki-delegates/src/main/kotlin/io/github/kerubistan/kroki/delegates/concurrency.kt
+++ kroki-delegates/src/main/kotlin/io/github/kerubistan/kroki/delegates/concurrency.kt
@@ -1,22 +1,1 @@
-package io.github.kerubistan.kroki.delegates
-
-import kotlin.reflect.KProperty
-
-private class FunctionLiteralThreadLocal<T>(private val initializer: () -> T) : ThreadLocal<T>() {
-	override fun initialValue(): T = initializer()
-}
-
-class ThreadLocalDelegate<T>(initializer: () -> T) {
-
-	private val threadLocal = FunctionLiteralThreadLocal(initializer)
-
-	operator fun getValue(obj: Any?, property: KProperty<*>): T = threadLocal.get()
-}
-
-/**
- * Allows having one instance created by thread.
- * This can be used to make efficient use of non-threadsafe classes, such as SimpleDateFormat or old XML parser API.
- * @param initializer a function literal that initializes the instance for the thread
- */
-fun <T> threadLocal(initializer: () -> T) = ThreadLocalDelegate(initializer)
-
+package io.github.kerubistan.kroki.delegatesimport kotlin.reflect.KPropertyprivate class FunctionLiteralThreadLocal<T>(private val initializer: () -> T) : ThreadLocal<T>() {	override fun initialValue(): T = initializer()}class ThreadLocalDelegate<T>(initializer: () -> T) {	private val threadLocal = FunctionLiteralThreadLocal(initializer)	operator fun getValue(obj: Any?, property: KProperty<*>): T = threadLocal.get()}class MutableThreadLocalDelegate<T>(initialValue: T?) {	constructor() : this(null)	private val threadLocal = FunctionLiteralThreadLocal(initializer = { initialValue })	operator fun getValue(obj: Any?, property: KProperty<*>): T? = threadLocal.get()	operator fun setValue(obj: Any?, property: KProperty<*>, newValue: T?) {		threadLocal.set(newValue)	}}/** * Allows having one instance created by thread. * This can be used to make efficient use of non-threadsafe classes, such as SimpleDateFormat or old XML parser API. * @param initializer a function literal that initializes the instance for the thread */fun <T> threadLocal(initializer: () -> T) = ThreadLocalDelegate(initializer)/** * Delegates the value to a ThreadLocal variable, which is initially null * and the value can be set later. */fun <T> threadLocal() = MutableThreadLocalDelegate<T>()/** * Delegates the value to a ThreadLocal variable with initial value, but can later be updated. * @param initialValue the initial value of the threadLocal - please remember that this will be on each new thread */fun <T> threadLocal(initialValue: T) = MutableThreadLocalDelegate(initialValue)
--- kroki-delegates/src/test/kotlin/io/github/kerubistan/kroki/delegates/DelegatesKtTest.kt
+++ kroki-delegates/src/test/kotlin/io/github/kerubistan/kroki/delegates/DelegatesKtTest.kt
@@ -1,207 +1,1 @@
-package io.github.kerubistan.kroki.delegates
-
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
-import org.junit.jupiter.api.Test
-import java.io.*
-import java.lang.Thread.yield
-import java.text.SimpleDateFormat
-import java.util.*
-import kotlin.concurrent.thread
-
-internal class DelegatesKtTest {
-
-	@Test
-	fun threadLocal() {
-		// GIVEN
-		val dateStr = "1969-07-16 13:32:00"
-		val dateFormat by threadLocal { SimpleDateFormat("yyyy-MM-dd hh:mm:ss") }
-		val results = mutableSetOf<Date>()
-
-		// WHEN
-		(1..1024).map {
-			thread {
-				val dates = (1..1024).map {
-					yield()
-					dateFormat.parse(dateStr)
-				}.toSet()
-				synchronized(results) {
-					results.addAll(dates)
-				}
-			}
-		}.map { it.join() }
-
-		//THEN
-		assertEquals(1, results.size)
-	}
-
-	@Test
-	fun weak() {
-
-		data class Image(val values: Int, val width: Int, val height: Int) {
-			val bitmap by weak {
-				Array(height) {
-					IntArray(width) { values }
-				}
-			}
-		}
-
-		val images = (1..8192).map { nr ->
-			Image(nr, width = 1024, height = 768)
-		}
-		images.forEach {
-			assertNotNull(it.bitmap)
-			it.bitmap.forEach { ints ->
-				assertNotNull(ints)
-			}
-		}
-
-		repeat(1024) {
-			assertNotNull(images.random().bitmap)
-		}
-
-	}
-
-	@Test
-	fun soft() {
-
-		data class Image(val values: Int, val width: Int, val height: Int) {
-			val bitmap by soft {
-				Array(height) {
-					IntArray(width) { values }
-				}
-			}
-		}
-
-		val images = (1..8192).map { nr ->
-			Image(nr, width = 1024, height = 768)
-		}
-		images.forEach {
-			assertNotNull(it.bitmap)
-			it.bitmap.forEach { ints ->
-				assertNotNull(ints)
-			}
-		}
-
-		repeat(1024) {
-			assertNotNull(images.random().bitmap)
-		}
-
-	}
-
-	private inline fun <reified T : Serializable> serializeDeserialize(obj: T): T {
-		val serialized = ByteArrayOutputStream().use { byteArrayOutputStream ->
-			ObjectOutputStream(byteArrayOutputStream).use {
-				it.writeObject(obj)
-			}
-			byteArrayOutputStream.toByteArray()
-		}
-
-		return ObjectInputStream(ByteArrayInputStream(serialized)).use { it.readObject() as T }
-	}
-
-	@Test
-	fun weakSerialization() {
-
-		data class BankAccount(
-			val accountId: String,
-			val balance: Double,
-			val currency: String
-		) : Serializable
-
-		data class Person(
-			val name: String,
-			val accounts: List<BankAccount> = listOf()
-		) : Serializable {
-			val bankAccountsByAccountId by weak { accounts.associateBy { it.accountId } }
-		}
-
-		val testPerson = Person(
-			name = "Bob",
-			accounts = listOf(
-				BankAccount(
-					accountId = "HUF00001234",
-					balance = 12345678.toDouble(),
-					currency = "HUF"
-				),
-				BankAccount(
-					accountId = "CHF00003456",
-					balance = 20000.toDouble(),
-					currency = "CHF"
-				)
-			)
-		)
-
-		val deserialized = serializeDeserialize(testPerson)
-
-		assertEquals(testPerson, deserialized)
-		assertEquals(
-			mapOf(
-				"HUF00001234" to BankAccount(
-					accountId = "HUF00001234",
-					balance = 12345678.toDouble(),
-					currency = "HUF"
-				),
-				"CHF00003456" to BankAccount(
-					accountId = "CHF00003456",
-					balance = 20000.toDouble(),
-					currency = "CHF"
-				)
-			),
-			deserialized.bankAccountsByAccountId
-		)
-	}
-
-	@Test
-	fun softSerialization() {
-
-		data class BankAccount(
-			val accountId: String,
-			val balance: Double,
-			val currency: String
-		) : Serializable
-
-		data class Person(
-			val name: String,
-			val accounts: List<BankAccount> = listOf()
-		) : Serializable {
-			val bankAccountsByAccountId by soft { accounts.associateBy { it.accountId } }
-		}
-
-		val testPerson = Person(
-			name = "Bob",
-			accounts = listOf(
-				BankAccount(
-					accountId = "HUF00001234",
-					balance = 12345678.toDouble(),
-					currency = "HUF"
-				),
-				BankAccount(
-					accountId = "CHF00003456",
-					balance = 20000.toDouble(),
-					currency = "CHF"
-				)
-			)
-		)
-
-		val deserialized = serializeDeserialize(testPerson)
-
-		assertEquals(testPerson, deserialized)
-		assertEquals(
-			mapOf(
-				"HUF00001234" to BankAccount(
-					accountId = "HUF00001234",
-					balance = 12345678.toDouble(),
-					currency = "HUF"
-				),
-				"CHF00003456" to BankAccount(
-					accountId = "CHF00003456",
-					balance = 20000.toDouble(),
-					currency = "CHF"
-				)
-			),
-			deserialized.bankAccountsByAccountId
-		)
-	}
-
-}
+package io.github.kerubistan.kroki.delegatesimport org.junit.jupiter.api.Assertions.assertEqualsimport org.junit.jupiter.api.Assertions.assertNotNullimport org.junit.jupiter.api.Testimport java.io.*import java.lang.Thread.yieldimport java.text.SimpleDateFormatimport java.util.*import kotlin.concurrent.threadimport kotlin.test.assertNullinternal class DelegatesKtTest {	@Test	fun threadLocalTest() {		// GIVEN		val dateStr = "1969-07-16 13:32:00"		val dateFormat by threadLocal { SimpleDateFormat("yyyy-MM-dd hh:mm:ss") }		val results = mutableSetOf<Date>()		// WHEN		(1..1024).map {			thread {				val dates = (1..1024).map {					yield()					dateFormat.parse(dateStr)				}.toSet()				synchronized(results) {					results.addAll(dates)				}			}		}.map { it.join() }		//THEN		assertEquals(1, results.size)	}	@Test	fun weak() {		data class Image(val values: Int, val width: Int, val height: Int) {			val bitmap by weak {				Array(height) {					IntArray(width) { values }				}			}		}		val images = (1..8192).map { nr ->			Image(nr, width = 1024, height = 768)		}		images.forEach {			assertNotNull(it.bitmap)			it.bitmap.forEach { ints ->				assertNotNull(ints)			}		}		repeat(1024) {			assertNotNull(images.random().bitmap)		}	}	@Test	fun soft() {		data class Image(val values: Int, val width: Int, val height: Int) {			val bitmap by soft {				Array(height) {					IntArray(width) { values }				}			}		}		val images = (1..8192).map { nr ->			Image(nr, width = 1024, height = 768)		}		images.forEach {			assertNotNull(it.bitmap)			it.bitmap.forEach { ints ->				assertNotNull(ints)			}		}		repeat(1024) {			assertNotNull(images.random().bitmap)		}	}	private inline fun <reified T : Serializable> serializeDeserialize(obj: T): T {		val serialized = ByteArrayOutputStream().use { byteArrayOutputStream ->			ObjectOutputStream(byteArrayOutputStream).use {				it.writeObject(obj)			}			byteArrayOutputStream.toByteArray()		}		return ObjectInputStream(ByteArrayInputStream(serialized)).use { it.readObject() as T }	}	@Test	fun weakSerialization() {		data class BankAccount(			val accountId: String,			val balance: Double,			val currency: String		) : Serializable		data class Person(			val name: String,			val accounts: List<BankAccount> = listOf()		) : Serializable {			val bankAccountsByAccountId by weak { accounts.associateBy { it.accountId } }		}		val testPerson = Person(			name = "Bob",			accounts = listOf(				BankAccount(					accountId = "HUF00001234",					balance = 12345678.toDouble(),					currency = "HUF"				),				BankAccount(					accountId = "CHF00003456",					balance = 20000.toDouble(),					currency = "CHF"				)			)		)		val deserialized = serializeDeserialize(testPerson)		assertEquals(testPerson, deserialized)		assertEquals(			mapOf(				"HUF00001234" to BankAccount(					accountId = "HUF00001234",					balance = 12345678.toDouble(),					currency = "HUF"				),				"CHF00003456" to BankAccount(					accountId = "CHF00003456",					balance = 20000.toDouble(),					currency = "CHF"				)			),			deserialized.bankAccountsByAccountId		)	}	@Test	fun softSerialization() {		data class BankAccount(			val accountId: String,			val balance: Double,			val currency: String		) : Serializable		data class Person(			val name: String,			val accounts: List<BankAccount> = listOf()		) : Serializable {			val bankAccountsByAccountId by soft { accounts.associateBy { it.accountId } }		}		val testPerson = Person(			name = "Bob",			accounts = listOf(				BankAccount(					accountId = "HUF00001234",					balance = 12345678.toDouble(),					currency = "HUF"				),				BankAccount(					accountId = "CHF00003456",					balance = 20000.toDouble(),					currency = "CHF"				)			)		)		val deserialized = serializeDeserialize(testPerson)		assertEquals(testPerson, deserialized)		assertEquals(			mapOf(				"HUF00001234" to BankAccount(					accountId = "HUF00001234",					balance = 12345678.toDouble(),					currency = "HUF"				),				"CHF00003456" to BankAccount(					accountId = "CHF00003456",					balance = 20000.toDouble(),					currency = "CHF"				)			),			deserialized.bankAccountsByAccountId		)	}	@Test	fun assignableThreadLocal() {		var x by threadLocal<String>()		assertNull(x)		x = "AAAA"		assertEquals("AAAA", x)		thread {			assertNull(x)			x = "BBBB"			assertEquals("BBBB", x)		}.join()		assertEquals("AAAA", x)	}	@Test	fun assignableThreadLocalWithInitialValue() {		var x by threadLocal("initial value")		assertEquals("initial value", x)		thread {			assertEquals("initial value", x)		}.join()		x = "updated value"		assertEquals("updated value", x)		thread {			assertEquals("initial value", x)		}.join()	}}
