eb281690fcc37924b630bd7e16df047135a7d8b2
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2021-11-11 17:58:00 +0100

    InputStream.peek implementation
    InputStreams with no support for mark use BufferedInputStream
    
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- kroki-utils/src/main/kotlin/io/github/kerubistan/kroki/io/iostreams.kt
+++ kroki-utils/src/main/kotlin/io/github/kerubistan/kroki/io/iostreams.kt
@@ -1,16 +1,20 @@
 package io.github.kerubistan.kroki.io
 
+import java.io.BufferedInputStream
 import java.io.InputStream
 
 fun InputStream.peek(process: (InputStream) -> Unit): InputStream {
-	return if(markSupported()) {
+	return if (markSupported()) {
 		mark(0)
 		process(this)
 		reset()
 		this
 	} else {
-		val fork = ForkInputStream(this)
-		process(fork)
-		ConcatenatedInputStream(listOf(fork.buffer.toByteArray().inputStream(), this))
+		BufferedInputStream(this).let {
+			it.mark(0)
+			process (it)
+			it.reset ()
+			it
+		}
 	}
 }
