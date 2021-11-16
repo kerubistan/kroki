2a678dc17a721bba112c3e2ee5c2e24f43726cac
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2021-11-16 07:31:05 +0100

    documentation and sample for InputStream.peek()
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- kroki-utils/src/main/kotlin/io/github/kerubistan/kroki/io/iostreams.kt
+++ kroki-utils/src/main/kotlin/io/github/kerubistan/kroki/io/iostreams.kt
@@ -3,6 +3,15 @@ package io.github.kerubistan.kroki.io
 import java.io.BufferedInputStream
 import java.io.InputStream
 
+/**
+ * Allows the program to read from the input in a function literal, make decisions
+ * and later still be able to read the whole input.
+ *
+ * @receiver the InputStream you want to peek into
+ * @param process the code that reads some of the input
+ * @return an input stream that is in the state like before the process peeked
+ * @sample io.github.kerubistan.kroki.io.IostreamsKtTest.peekSample
+ */
 fun InputStream.peek(process: (InputStream) -> Unit): InputStream {
 	return if (markSupported()) {
 		mark(0)
@@ -12,8 +21,8 @@ fun InputStream.peek(process: (InputStream) -> Unit): InputStream {
 	} else {
 		BufferedInputStream(this).let {
 			it.mark(0)
-			process (it)
-			it.reset ()
+			process(it)
+			it.reset()
 			it
 		}
 	}
--- kroki-utils/src/test/kotlin/io/github/kerubistan/kroki/io/IostreamsKtTest.kt
+++ kroki-utils/src/test/kotlin/io/github/kerubistan/kroki/io/IostreamsKtTest.kt
@@ -1,12 +1,56 @@
 package io.github.kerubistan.kroki.io
 
-import io.github.kerubistan.kroki.strings.substringBetween
+import com.fasterxml.jackson.core.JsonParseException
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.times
+import com.nhaarman.mockito_kotlin.verify
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
+import java.io.InputStream
 import java.nio.charset.StandardCharsets.UTF_8
 
 internal class IostreamsKtTest {
+
+	@Test
+	fun peekSample() {
+		abstract class InputProcessor() {
+			abstract fun process(stream : InputStream)
+		}
+		val jsonProcessor = mock<InputProcessor>()
+		val xmlProcessor = mock<InputProcessor>()
+		listOf(
+			"""<text>
+					hello world!
+				</text>
+			""".trimIndent(),
+			"""{
+					"text": "hello world!"
+				}
+			""".trimIndent()
+		).map { it.byteInputStream(UTF_8) }.forEach {
+			stream ->
+			var isJson = false
+			stream.peek {
+				try {
+					ObjectMapper().createParser(stream).nextToken()
+					isJson = true
+				} catch (e : JsonParseException) {
+					println("beee, not json!")
+				}
+			}.use {
+				input ->
+				if(isJson) {
+					jsonProcessor.process(input)
+				} else {
+					xmlProcessor.process(input)
+				}
+			}
+		}
+		verify(jsonProcessor, times(1)).process(any())
+		verify(xmlProcessor, times(1)).process(any())
+	}
 
 	@Test
 	fun peek() {
