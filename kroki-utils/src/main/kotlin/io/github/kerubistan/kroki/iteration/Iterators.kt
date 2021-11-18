7ca5e562438ff65447245279c712ff5b349888aa
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2021-11-18 16:47:44 +0100

    documentation and samples
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- kroki-utils/src/main/kotlin/io/github/kerubistan/kroki/iteration/Iterators.kt
+++ kroki-utils/src/main/kotlin/io/github/kerubistan/kroki/iteration/Iterators.kt
@@ -1,6 +1,9 @@
 package io.github.kerubistan.kroki.iteration
 
-class MappingIterator<S, T>(private val internal: Iterator<S>, private val mapper: (S) -> T) : Iterator<T> {
+/**
+ * @suppress
+ */
+internal class MappingIterator<S, T>(private val internal: Iterator<S>, private val mapper: (S) -> T) : Iterator<T> {
 
 	override fun hasNext(): Boolean = internal.hasNext()
 
@@ -8,8 +11,20 @@ class MappingIterator<S, T>(private val internal: Iterator<S>, private val mappe
 
 }
 
+/**
+ * Creates an iterator over a collection that transforms the objects while iterating.
+ * @receiver the collection to transform while iterating
+ * @param mapper the transformation function literal
+ * @return an iterator over the transformed objects
+ * @sample io.github.kerubistan.kroki.iteration.IteratorsKtTest.iteratorToMap
+ */
 fun <S, T> Iterator<S>.map(mapper: (S) -> T): Iterator<T> = MappingIterator(this, mapper)
 
+/**
+ * Builds a map out of a iterator using a item to key-value pair transform function literal.
+ * @param map transformation from the item type to pair of key and value
+ * @sample io.github.kerubistan.kroki.iteration.IteratorsKtTest.iteratorToMap
+ */
 fun <T, K, V> Iterator<T>.toMap(map: T.() -> Pair<K, V>): Map<K, V> = mutableMapOf<K, V>().let { target ->
 	this.forEach {
 		val (key, value) = it.map()
@@ -19,7 +34,9 @@ fun <T, K, V> Iterator<T>.toMap(map: T.() -> Pair<K, V>): Map<K, V> = mutableMap
 }
 
 /**
- * Builds a list in case only an iterator is available.s
+ * Builds a list in case only an iterator is available.
+ * @receiver the iterator out of which the list will be built
+ * @sample io.github.kerubistan.kroki.iteration.IteratorsKtTest.toList
  */
 fun <T> Iterator<T>.toList(): List<T> = mutableListOf<T>().apply {
 	while (this@toList.hasNext()) {
