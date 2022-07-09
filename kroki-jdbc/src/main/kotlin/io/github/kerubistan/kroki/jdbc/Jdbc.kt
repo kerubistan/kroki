7cabdb1eeb6cc198467e79a29eff18ac30237c7b
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2022-07-09 23:25:10 +0200

    documentation for jdbc module
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- kroki-jdbc/src/main/kotlin/io/github/kerubistan/kroki/jdbc/Jdbc.kt
+++ kroki-jdbc/src/main/kotlin/io/github/kerubistan/kroki/jdbc/Jdbc.kt
@@ -6,6 +6,11 @@ import java.sql.Connection
 import java.sql.ResultSet
 import javax.sql.DataSource
 
+/**
+ * Borrow a connection from the datasource, execute operations on it and return the connection to the pool.
+ * @param function operations to perform
+ * @sample io.github.kerubistan.kroki.jdbc.JdbcSamples.dataSourceUseSample
+ */
 inline fun <T> DataSource.use(function: Connection.() -> T): T = this.connection.let {
 	try {
 		it.function()
@@ -14,14 +19,35 @@ inline fun <T> DataSource.use(function: Connection.() -> T): T = this.connection
 	}
 }
 
+/**
+ * Borrow a connection to execute a single query on it, and then return the connection.
+ * @param query the SQL query
+ * @param mapper the result mapper
+ * @return list of mapped results
+ */
 inline fun <T> DataSource.query(query: String, crossinline mapper: ResultSet.() -> T) : List<T> =
 	this.use {
 		query(query, mapper)
 	}
 
+/**
+ * Query using the connection with a simple, non-parameterized query and map the results into a list.
+ * @param query the SQL query
+ * @param mapper the result mapper
+ * @return list of mapped results
+ * @sample io.github.kerubistan.kroki.jdbc.JdbcSamples.connectionQuerySample
+ */
 inline fun <R> Connection.query(query: String, crossinline mapper: ResultSet.() -> R) =
 	this.createStatement().use { executeQuery(query).use { toList(mapper) } }
 
+/**
+ * Query using the connection with a parameterized query, map the results and close the resources used.
+ * @param query the parameterized SQL query
+ * @param params the parameters to the query
+ * @param mapper the result mapper
+ * @return the list of mapped results
+ * @sample io.github.kerubistan.kroki.jdbc.JdbcSamples.connectionParameterizedQuerySample
+ */
 inline fun <R> Connection.query(query: String, vararg params: Any, crossinline mapper: ResultSet.() -> R): List<R> =
 	this.prepareStatement(query).use {
 		params.forEachIndexed { index, param ->
---
+++ kroki-jdbc/src/test/kotlin/io/github/kerubistan/kroki/jdbc/JdbcSamples.kt
@@ -0,0 +1,38 @@
+package io.github.kerubistan.kroki.jdbc
+
+import java.sql.Connection
+import java.sql.Date
+import javax.sql.DataSource
+
+class JdbcSamples {
+	fun dataSourceUseSample(dataSource: DataSource) {
+		dataSource.use {
+			query("select sysdate from dual") {
+				getDate(1)
+			}
+		}
+	}
+	fun connectionQuerySample(connection: Connection) {
+		data class Employee(val id : Long, val name: String, val dateOfBirth : Date)
+		connection.query("select id, name, date_of_birth from employees") {
+			Employee(
+				id = getLong("id"),
+				name = getString("name"),
+				dateOfBirth = getDate("date_of_birth")
+			)
+		}
+	}
+	fun connectionParameterizedQuerySample(connection: Connection) {
+		data class Employee(val id : Long, val name: String, val dateOfBirth : Date)
+		connection.query(
+			"select id, name, date_of_birth from employees where name like ?",
+			"Bob%"
+		) {
+			Employee(
+				id = getLong("id"),
+				name = getString("name"),
+				dateOfBirth = getDate("date_of_birth")
+			)
+		}
+	}
+}
