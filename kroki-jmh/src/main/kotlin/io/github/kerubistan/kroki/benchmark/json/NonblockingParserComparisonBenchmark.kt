fdab9c2a83dae618f9dcceac59f6bdc16c7f2ce2
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2023-06-08 06:45:58 +0200

    benchmark for blocking vs nio parser comparison
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

---
+++ kroki-jmh/src/main/kotlin/io/github/kerubistan/kroki/benchmark/json/NonblockingParserComparisonBenchmark.kt
@@ -0,0 +1,72 @@
+package io.github.kerubistan.kroki.benchmark.json
+
+import com.fasterxml.jackson.core.JsonToken
+import com.fasterxml.jackson.core.json.async.NonBlockingJsonParser
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.Param
+import org.openjdk.jmh.annotations.Scope
+import org.openjdk.jmh.annotations.Setup
+import org.openjdk.jmh.annotations.State
+import org.openjdk.jmh.annotations.TearDown
+import org.openjdk.jmh.infra.Blackhole
+import java.io.File
+import java.nio.ByteBuffer
+import java.util.*
+
+@State(Scope.Benchmark)
+open class NonblockingParserComparisonBenchmark {
+
+	@Param("1", "4", "16", "64", "256", "1024", "4096")
+	var size : Int = 1
+	private val objectMapper = ObjectMapper()
+
+	lateinit var testFile : File
+
+	@Setup
+	fun setup() {
+		testFile = File.createTempFile(UUID.randomUUID().toString(), ".json")
+		testFile.writer().use { writer ->
+			objectMapper.writeValue(writer,
+				(0 until size).associate { nr -> "item-$nr" to "value-$nr" }
+			)
+		}
+	}
+
+	@TearDown
+	fun cleanup() {
+		testFile.delete()
+	}
+
+	@Benchmark
+	fun blockingParse(hole : Blackhole) {
+		objectMapper.createParser(testFile).let {
+			var lastToken = it.nextToken()
+			while (lastToken != JsonToken.END_OBJECT) {
+				lastToken = it.nextToken()
+			}
+		}
+	}
+
+	@Benchmark
+	fun nonBlockingParse(hole : Blackhole) {
+		val byteBuffer = ByteBuffer.allocate(4096)
+		ObjectMapper().createNonBlockingByteArrayParser().use {
+			parser ->
+			parser as NonBlockingJsonParser
+			testFile.inputStream().channel.use {
+				channel ->
+				while (channel.position() < channel.size()) {
+					val bufferSize = channel.read(byteBuffer)
+					val array = byteBuffer.array()
+					parser.feedInput(array, 0, bufferSize)
+					while (!parser.nonBlockingInputFeeder.needMoreInput()) {
+						parser.nextToken()
+					}
+					byteBuffer.flip()
+				}
+			}
+		}
+	}
+
+}
