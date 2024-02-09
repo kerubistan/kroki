7d30aee3981c31adc50e191b27115738860de440
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2024-02-09 21:17:09 +0100

    benchmark for sorting somewhat ordered lists
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

---
+++ kroki-jmh/src/main/kotlin/io/github/kerubistan/kroki/benchmark/collections/jvm/ResortBenchmark.kt
@@ -0,0 +1,62 @@
+package io.github.kerubistan.kroki.benchmark.collections.jvm
+
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.Param
+import org.openjdk.jmh.annotations.Scope
+import org.openjdk.jmh.annotations.Setup
+import org.openjdk.jmh.annotations.State
+import org.openjdk.jmh.infra.Blackhole
+import java.util.*
+
+@State(Scope.Benchmark)
+open class ResortBenchmark {
+
+	@Param("10", "100", "1000")
+	var listSize = 10
+
+	@Param("array", "linked")
+	var implementation = "array"
+
+	var sortedList = listOf<Int>()
+
+	var shuffledList = listOf<Int>()
+
+	var singleItemChanged = listOf<Int>()
+
+	@Setup
+	fun setup() {
+		sortedList = (0..listSize).toList()
+		shuffledList = sortedList.shuffled()
+		singleItemChanged = sortedList.map {
+			if (it == 0) {
+				2
+			} else it
+		}
+		when (implementation) {
+			"array" -> {
+
+			}
+
+			"linked" -> {
+				sortedList = LinkedList(sortedList)
+				shuffledList = LinkedList(shuffledList)
+				singleItemChanged = LinkedList(singleItemChanged)
+			}
+		}
+	}
+
+	@Benchmark
+	fun resortOrderedList(hole: Blackhole) {
+		hole.consume(sortedList.sorted())
+	}
+
+	@Benchmark
+	fun resortShuffledList(hole: Blackhole) {
+		hole.consume(shuffledList.sorted())
+	}
+
+	@Benchmark
+	fun resortSingleItem(hole: Blackhole) {
+		hole.consume(singleItemChanged.sorted())
+	}
+}
