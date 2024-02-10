96d7003e7ce7b869f8d075e39c62bf053ae2c919
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2024-02-10 18:43:52 +0100

    shuffle channel
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

---
+++ kroki-coroutines/src/main/kotlin/io/github/kerubistan/kroki/coroutines/channels/Shuffle.kt
@@ -0,0 +1,27 @@
+package io.github.kerubistan.kroki.coroutines.channels
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.produce
+import java.util.LinkedList
+import kotlin.random.Random
+
+fun <T> CoroutineScope.shuffle(
+	channel: ReceiveChannel<T>,
+	capacity: Int,
+	outCapacity: Int = 4,
+	random : Random = Random.Default
+): ReceiveChannel<T> = produce(capacity = outCapacity) {
+	val buffer = LinkedList<T>()
+	for (message in channel) {
+		if(buffer.size >= capacity) {
+			val index = random.nextInt(buffer.size)
+			send(buffer.removeAt(index))
+		}
+		buffer.add(message)
+	}
+	while (buffer.isNotEmpty()) {
+		val index = random.nextInt(buffer.size)
+		send(buffer.removeAt(index))
+	}
+}
---
+++ kroki-coroutines/src/test/kotlin/io/github/kerubistan/kroki/coroutines/channels/ShuffleKtTest.kt
@@ -0,0 +1,23 @@
+package io.github.kerubistan.kroki.coroutines.channels
+
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class ShuffleKtTest {
+
+	@Test
+	fun shuffle() = runBlocking {
+		val results = mutableListOf<Int>()
+		for (message in shuffle(
+			produce { (1..100).forEach { send(it) } },
+			capacity = 10
+		)) {
+			results.add(message)
+		}
+
+		assertEquals((1..100).toList(), results.sorted())
+	}
+}
