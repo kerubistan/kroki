c6e0aa2e9a801c125c184190e6328acb01e1bc73
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2024-02-10 18:49:36 +0100

    tests for sortedList
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

---
+++ kroki-coroutines/src/test/kotlin/io/github/kerubistan/kroki/coroutines/channels/SortedListTest.kt
@@ -0,0 +1,48 @@
+package io.github.kerubistan.kroki.coroutines.channels
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class SortedListTest {
+
+	@Test
+	fun isEmpty() {
+		val list = SortedList(listOf<Int>()) { first, second -> first.compareTo(second) }
+
+		assertTrue(list.isEmpty())
+		assertFalse(list.isNotEmpty())
+	}
+
+	@Test
+	fun construct() {
+		val list = SortedList(listOf(3, 1, 2)) { first, second -> first.compareTo(second) }
+		assertFalse(list.isEmpty())
+		assertEquals(3, list.removeFirst())
+		assertFalse(list.isEmpty())
+		assertEquals(2, list.removeFirst())
+		assertFalse(list.isEmpty())
+		assertEquals(1, list.removeFirst())
+		assertTrue(list.isEmpty())
+	}
+
+	@Test()
+	fun removeFirstEmptyList() {
+		val list = SortedList(listOf<Int>()) { first, second -> first.compareTo(second) }
+		assertThrows(IllegalStateException::class.java) {
+			list.removeFirst()
+		}
+	}
+
+	@Test
+	fun add() {
+		val list = SortedList(listOf<Int>()) { first, second -> first.compareTo(second) }
+
+		list.add(2)
+		list.add(1)
+		list.add(3)
+
+		assertEquals(3, list.removeFirst())
+		assertEquals(2, list.removeFirst())
+		assertEquals(1, list.removeFirst())
+	}
+}
