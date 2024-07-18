38dda36b2df45b5e30a87fb100e6ddaec3d372db
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2024-07-18 17:47:17 +0200

    pre-allocation of list capacity in batch
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- kroki-coroutines/src/main/kotlin/io/github/kerubistan/kroki/coroutines/channels/Batch.kt
+++ kroki-coroutines/src/main/kotlin/io/github/kerubistan/kroki/coroutines/channels/Batch.kt
@@ -1,17 +1,19 @@
 package io.github.kerubistan.kroki.coroutines.channels
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.produce
 
+@OptIn(ExperimentalCoroutinesApi::class)
 suspend fun <T> CoroutineScope.batch(channel: ReceiveChannel<T>, chunkSize: Int): ReceiveChannel<List<T>> =
 	produce {
-		var list = mutableListOf<T>()
+		var list = ArrayList<T>(chunkSize)
 		for (message in channel) {
 			list.add(message)
 			if (list.size >= chunkSize) {
 				send(list)
-				list = mutableListOf()
+				list = ArrayList(chunkSize)
 			}
 		}
 		if (list.isNotEmpty()) {
---
+++ kroki-jmh/src/main/kotlin/io/github/kerubistan/kroki/benchmark/coroutines/channels/BatchBenchmark.kt
@@ -0,0 +1,33 @@
+package io.github.kerubistan.kroki.benchmark.coroutines.channels
+
+import io.github.kerubistan.kroki.coroutines.channels.batch
+import io.github.kerubistan.kroki.coroutines.channels.sink
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.runBlocking
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.Param
+import org.openjdk.jmh.annotations.Scope
+import org.openjdk.jmh.annotations.State
+
+@State(Scope.Benchmark)
+open class BatchBenchmark {
+
+	@Param("1", "4", "16", "32", "64", "128", "256", "512", "1024")
+	var size : Int = 1
+
+	@Param("256", "512", "1024")
+	var count : Int = 1
+
+	@OptIn(ExperimentalCoroutinesApi::class)
+	@Benchmark
+	fun run() = runBlocking {
+		val input = produce {
+			repeat(count) {
+				send(it)
+			}
+		}
+		val batches = batch(input, size)
+		sink(batches)
+	}
+}
