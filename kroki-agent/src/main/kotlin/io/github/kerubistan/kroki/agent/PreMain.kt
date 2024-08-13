8efcf6a21dac244f3e88cbab55e5954f93ccf8e8
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2024-08-13 07:50:08 +0200

    Added bytebuddy dependency
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- kroki-agent/src/main/kotlin/io/github/kerubistan/kroki/agent/PreMain.kt
+++ kroki-agent/src/main/kotlin/io/github/kerubistan/kroki/agent/PreMain.kt
@@ -1,12 +1,14 @@
 package io.github.kerubistan.kroki.agent
 
+import net.bytebuddy.agent.builder.AgentBuilder
 import java.lang.instrument.Instrumentation
 
 class PreMain {
 	companion object {
 		@JvmStatic
-		fun premain(argument : String?, instrumentation: Instrumentation?) {
-			println("fortune, fame, mirror vain, gone insane but the memory premains")
+		fun premain(argument: String?, instrumentation: Instrumentation) {
+			AgentBuilder.Default()
+				.installOn(instrumentation)
 		}
 	}
 }
